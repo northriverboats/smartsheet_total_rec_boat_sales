84647d1db4d809482699b3eeb78414122a1ccad5
Fredrick W. Warren <fredw@northriverboats.com>
2021-11-09 09:52:48 -0800

    improved commenting

--- smartsheet_total_rec_boat_sales.py
+++ smartsheet_total_rec_boat_sales.py
@@ -8,7 +8,14 @@ from dotenv import load_dotenv
 import smartsheet  # type: ignore
 
 def resource_path(relative_path: str) -> str:
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """Get absolute path to resource, works for dev and for PyInstaller
+
+    Arguments:
+        relative_path str -- releative path from current directory + file name
+
+    Returns:
+        str -- absoulte path to file
+    """
     try:
         # pylint: disable=protected-access
         base_path: str = sys._MEIPASS  # type: ignore
@@ -30,6 +37,7 @@ def main()-> None:
 
     file_name: str = datetime.now().strftime(sheet_name)
     smart.Reports.get_report_as_excel(report_id, '/tmp', file_name)
+    sys.exit()
 
 
 if __name__ == "__main__":
